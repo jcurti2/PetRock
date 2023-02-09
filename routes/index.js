f2d15659c87591c78e2f52104cc6d8f8829a5047
jcurti2 <joshua.curtis24@gmail.com>
2023-02-09 11:46:30 -0800

    create new rock controller and route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
+const controllers = require('../controllers')
 const router = Router();
 
 router.get('/', (req, res) => res.send('this is earths core!!!'))
+
+router.post('/rocks', controllers.digUpNewRock)
 
 module.exports = router;
