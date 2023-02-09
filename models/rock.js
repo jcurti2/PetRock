f41b5059593bc4e7e110dd769520df667384492e
jcurti2 <joshua.curtis24@gmail.com>
2023-02-09 10:04:55 -0800

    rock schema

---
+++ models/rock.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Rock = new Schema(
+    {
+        name: {type: String, required:true},
+        image: { type: String, required: true},
+        rarity: {type: Number, required: true},
+        cost: {type: Number, requied: true},
+        owner_id:{type: Schema.Types.ObjectId, ref: 'owner_id'} 
+    },
+    { timestamps: true }
+)
+
+module.exports = Rock
