b125cd3e7cf9a5c02eb313794034c5b97ad0da7c
jcurti2 <joshua.curtis24@gmail.com>
2023-02-10 09:04:38 -0800

    app created

---
+++ pet-rock-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Routes>
+          {/* <Route path='/' element={<Home/>} /> */}
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ pet-rock-app/src/pages/Home.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+//const will be created here
+
+
+//functions created here
+
+
+
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
