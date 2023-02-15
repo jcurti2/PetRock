53b6e8f30e995faced1e3c371d4b789d80e12267
jcurti2 <joshua.curtis24@gmail.com>
2023-02-14 16:25:54 -0800

    end of day tuesday

--- Client/src/components/RockComponent.jsx
+++ Client/src/components/RockComponent.jsx
@@ -13,16 +13,6 @@ const RockComponent = (props) => {
 
   const [randRock, setRandRock] = useState([])
 
-  // const [updateRock, setUpdateRock] = useState({})
-
-  const [ownerRockId, setownerRockId] = useState('')
-
-  const [editing, setEditing] = useState(true)
-
-
-
-  
-
   const getRock = async () => {
     const temp = await axios.get(`http://localhost:3001/api/rocks/`)
     setRock(temp.data.rocks)
@@ -31,14 +21,18 @@ const RockComponent = (props) => {
 function rockGenerator(){
  const arr = [] 
     for(let i = 0; i < 5; i++){
-
+      let name =Math.floor( [1 + Math.random() * ((rockData.names.length) - 1)]) 
+      let randoName=rockData.names[name]
+      let picture=Math.floor([1 + Math.random() * ((rockData.picture.length) - 1)])
+      let randomPic=rockData.picture[picture]
   let temp = {
     
-    "name": "Anorthosite",
-    "picture": rockData.picture[1 + Math.random() * ((rockData.picture.length) - 1)], 
+    "name":randoName,
+    "picture": randomPic, 
     "rarity": 1 + Math.random() * (100 - 1),
     "cost": 1 + Math.random() * (1000 - 1),
     }
+    
     arr.push(temp)
   }   
     setRandRock(arr)
@@ -49,15 +43,6 @@ const sellRock = async ( id ) => {
   await axios.delete(`http://localhost:3001/api/rocks/${id}`)
   getRock();
 }
-
-
-
-
-
-// const handleEditState = (rock) => { 
-//     setUpdateRock({name:`${rock.name}`})
-//     setownerRockId(rock._id)
-// }
 
 
 useEffect(() => {
@@ -91,19 +76,6 @@ useEffect(()=> {
     
        <div key={singleRock._id}> <EditRock rock={singleRock} getRock={getRock}/>Name: {singleRock.name}       
         <button onClick={() => sellRock(singleRock._id)}>Sell Rock</button>
-        
-        {/* <form onSubmit = {(e) => handleSubmit(e, singleRock._id )}>
-        <input 
-            type='text'
-            value={singleRock.name}
-            placeholder="Rock Name"
-            onChange={handleChange}
-            >
-          </input>
-          <button onClick={() => 
-            handleEditState(singleRock)
-          } type='submit'>ChangeName</button>
-          </form> */}
        </div> 
       
     )))}
